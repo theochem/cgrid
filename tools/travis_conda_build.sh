e3f928d7ce214985ccf5a7c287686a49332c4b97
Matthew Chan <chanmhy@mcmaster.ca>
2017-09-01 18:41:38 +0200

    keep cache clean

--- tools/travis_conda_build.sh
+++ tools/travis_conda_build.sh
@@ -14,3 +14,7 @@ conda install --use-local ${CONDA_PKG_NAME_CPP} ${CONDA_PKG_NAME_PY}
 # Run the unittests, using the installed package
 ${CONDA_PREFIX}/libexec/${PROJECT_NAME}/test_${PROJECT_NAME}
 (cd; nosetests ${PROJECT_NAME} -v --detailed-errors)
+
+# Keep miniconda cache clean
+conda uninstall ${CONDA_PKG_NAME_PY}
+conda uninstall ${CONDA_PKG_NAME_CPP}
