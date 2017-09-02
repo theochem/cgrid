06ac732cbb126c3ea351b2ff618901e8939a0921
Matthew Chan <chanmhy@mcmaster.ca>
2017-09-02 14:33:53 +0200

    use conda for both debug and release builds

--- tools/conda.recipe/build.sh
+++ tools/conda.recipe/build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [[ ${BUILD_TYPE} ]]; then
+  BTYPE=${BUILD_TYPE}
+else
+  BTYPE="release"
+fi
+
 mkdir build
 cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=release \
+  -DCMAKE_BUILD_TYPE=${BTYPE} \
   ..
 make install
