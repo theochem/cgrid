fdaf90509a49f5bdc4aeed01862e38d50168f764
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-09-03 01:06:52 +0200

    Remove outdated scripts

--- tools/checkcode.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./tools/cpplint.py --linelength=90 qcgrids/*.cpp qcgrids/*.h
-./tools/cpplint.py --linelength=120 qcgrids/tests/*.cpp qcgrids/tests/*.h
--- tools/coverage_clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -fvr gcov
-find -type f | grep "gcda$" | xargs rm -vf
